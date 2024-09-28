c5ca591ff18df6759a2b0a33fd30dd39165cf972
mostlybadfly <reachdanielnunez@gmail.com>
2024-09-28 14:29:27 -0500

    edit post link

--- client/src/features/posts/PostDetails.jsx
+++ client/src/features/posts/PostDetails.jsx
@@ -29,6 +29,8 @@ function PostDetails() {
     <div>
       <h2>{post.title}</h2>
       <p>{post.body}</p>
+      <Link to={`/posts/${id}/edit`}>Edit</Link>
+      {" | "}
       <Link to="/">Back to Posts</Link>
     </div>
   );
